c8942257d4060c996e75d24e86bdaedc9b809a3b
amal-j-antony <271721431+amal-j-antony@users.noreply.github.com>
2026-07-07 22:22:49 +0530

    tailwind and shadcn setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './common/Header'
 
 function App() {
   
 
   return (
     <>
+    <Header/>
      <h1>APP</h1>
     </>
   )
