477502018a2668a37c2e1d63b00d70319fe6055b
amal-j-antony <271721431+amal-j-antony@users.noreply.github.com>
2026-07-11 21:57:46 +0530

    update gitignore, config render

---
+++ client/src/services/candidateAPI.js
@@ -0,0 +1,6 @@
+import { serverURL } from "./serverURL";
+
+//get candidate data 
+export const getCandidateAPI = async(accountID) => {
+    
+}
--- client/src/services/serverURL.js
+++ client/src/services/serverURL.js
@@ -1,1 +1,3 @@
-export const serverURL = 'http://localhost:3000'
+
+
+export const serverURL = import.meta.env.VITE_BASE_URL
